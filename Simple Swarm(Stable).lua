c66c3216d9fdebd532d2f4859a5d9c485458c8d4
DivineEntity01 <78000390+DivineEntity01@users.noreply.github.com>
2021-02-05 14:28:29 -0600

    Update Simple Swarm(Stable).lua

--- Simple Swarm(Stable).lua
+++ Simple Swarm(Stable).lua
@@ -101,18 +101,20 @@ local f1, f2, f3=
 {[1] = "Blue Field Booster"},
 {[1] = "Field Booster"}
 local d = {"Bamboo", "strawberry", "Pumpkin", "Cactus", "Sundower", "Pineapple", "Pine Tree", "Big Pine Tree", "Mushroom"}
-                                local t = {
-                    "rbxassetid://2306224708", --Mooncharm
-                    "rbxassetid://2545746569", --Oil
-                    "rbxassetid://1674686518", --Ticket
-                    "rbxassetid://1674871631", --Ticket
-                    "rbxassetid://2504978518", --Glue
-                    "rbxassetid://2584584968", --Enzymes
-                    "rbxassetid://1629547638", --Token link
-                    "rbxassetid://2319943273", --Starjelly
-                    "rbxassetid://2000457501", --Inspire Token
-                    "rbxassetid://177997841",  --Glob Token
-                    "rbxassetid://1839454544"  -- Gumdrop Barrage
+local t = {
+"rbxassetid://2529092020", --Magic Bean
+"rbxassetid://2545746569", --Oil
+"rbxassetid://2495936068", --Blue Extract
+"rbxassetid://2495935302", --Red Extract
+"rbxassetid://2306224708", --Mooncharm
+"rbxassetid://2545746569", --Oil
+"rbxassetid://1674686518", --Ticket
+"rbxassetid://1674871631", --Ticket
+"rbxassetid://2504978518", --Glue
+"rbxassetid://2584584968", --Enzymes
+"rbxassetid://2319943273", --Starjelly
+"rbxassetid://2000457501", --Inspire Token
+"rbxassetid://2542899802"  --Glitter 
 }
 
 
